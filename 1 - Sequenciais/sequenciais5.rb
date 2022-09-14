803451306cb2a5e2ef3b6f14620497717debfdeb
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2022-09-14 14:49:31 -0300

    Delete 1 - Sequenciais directory

--- 1 - Sequenciais/sequenciais5.rb
+++
@@ -1,9 +0,0 @@
-#Faça um programa que converta metros para centímetros
-
-# Author:  Igor Vinicius Freitas de Souza
-# GitHub: https://github.com/igor1043
-# E-mail: igorviniciusfreitasouza@gmail.com
-
-puts "Digite um tamanho, em metros: "
-tamanho = gets.to_f
-puts "O tamanho informado, em centímetros, é de: #{tamanho * 100}"
